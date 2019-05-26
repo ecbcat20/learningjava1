ae05edcbedd479635ebb20d78f12b810e1c1cf9d
Skye <ecbcat20@gmail.com>
2019-05-26 10:39:24 -0400

    Merging shit
    description

---
+++ gettingbacktocode/src/functions/forLoops.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package functions;
+
+/**
+ * @author evanb
+ *
+ */
+public class forLoops {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
